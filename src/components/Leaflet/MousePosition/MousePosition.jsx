f3a81266dbba46007004d8b379c03cd8daa9ae39
Alex Cornellier <alex@jintronix.com>
2023-09-10 19:01:45 -0400

    fix mouseposition bug

--- src/components/Leaflet/MousePosition/MousePosition.jsx
+++ src/components/Leaflet/MousePosition/MousePosition.jsx
@@ -1,9 +1,14 @@
 import { useMap } from 'react-leaflet'
 import './L.MousePosition.js'
 import L from 'leaflet'
+import { useEffect } from 'react'
 
 export function MousePosition() {
   const map = useMap()
-  L.control.mousePosition().addTo(map)
+
+  useEffect(() => {
+    L.control.mousePosition().addTo(map)
+  }, [])
+
   return null
 }
