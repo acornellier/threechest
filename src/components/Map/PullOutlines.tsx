acabda96bac8b7691e0fb2d14c6e172e2f465d17
Alex Cornellier <alex@jintronix.com>
2024-02-27 23:58:48 -0500

    pull #

--- src/components/Map/PullOutlines.tsx
+++ src/components/Map/PullOutlines.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from '../RouteContext/UseRoute.ts'
 import { useEffect, useMemo, useState } from 'react'
-import { Polygon, useMap } from 'react-leaflet'
+import { Polygon, Tooltip, useMap } from 'react-leaflet'
 import makeHull from 'hull.js'
 import Offset from 'polygon-offset'
 
@@ -8,11 +8,11 @@ export function PullOutlines() {
   const map = useMap()
   const { route } = useRoute()
 
+  // Change key to force re-render
   const [foo, setFoo] = useState(0)
-
   useEffect(() => {
     setFoo((foo) => foo + 1000)
-  }, [route.selectedPull, route.hoveredPull])
+  }, [route.selectedPull, route.hoveredPull, route.pulls])
 
   const convexHulls = useMemo(() => {
     return route.pulls.map((pull) => {
@@ -25,13 +25,15 @@ export function PullOutlines() {
 
       const arcSegments = Math.max(5, 9 - hull.length + 2 * map.getZoom())
       const margin = 2.8
-      hull = new Offset().data(hull).arcSegments(arcSegments).margin(margin)
+      hull = new Offset().data(hull).arcSegments(arcSegments).margin(margin)[0]
 
       return { pull, hull }
     })
   }, [map, route.pulls])
 
   return convexHulls.map(({ pull, hull }, idx) => {
+    if (hull.length <= 1) return null
+
     const isHovered = route.hoveredPull === idx
     const isSelected = route.selectedPull === idx
     return (
@@ -42,7 +44,11 @@ export function PullOutlines() {
         fillOpacity={0}
         opacity={isSelected || isHovered ? 1 : 0.6}
         weight={isSelected ? 6 : isHovered ? 5 : 3.5}
-      />
+      >
+        <Tooltip className="pull-number-tooltip" direction="center" permanent>
+          {idx + 1}
+        </Tooltip>
+      </Polygon>
     )
   })
 }
--- src/typings/polygon-offset.ts
+++ src/typings/polygon-offset.ts
@@ -2,7 +2,7 @@ declare module 'polygon-offset' {
   class Offset {
     data: (points: Array<[number, number]>) => Offset
     arcSegments: (segments: number) => Offset
-    margin: (margin: number) => Array<[number, number]>
+    margin: (margin: number) => Array<Array<[number, number]>>
   }
 
   export = Offset
