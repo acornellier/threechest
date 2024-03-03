758d85b57a392e19d83710b11d5b403cbec17672
Alex Cornellier <alex@jintronix.com>
2024-03-03 16:39:56 -0500

    scale icons while zooming

--- src/components/Map/Mobs.tsx
+++ src/components/Map/Mobs.tsx
@@ -1,5 +1,5 @@
 import { useMap, useMapEvent } from 'react-leaflet'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { mapIconScaling } from '../../code/map.ts'
 import { useDungeon } from '../../store/hooks.ts'
 import { MobSpawn } from './MobSpawn.tsx'
@@ -9,7 +9,15 @@ export function Mobs() {
   const dungeon = useDungeon()
   const map = useMap()
 
+  const prevZoom = useRef(map.getZoom())
   const [iconScaling, setIconScaling] = useState(mapIconScaling(map))
+
+  useMapEvent('zoom', () => {
+    if (Math.abs(prevZoom.current - map.getZoom()) > 0.3) {
+      prevZoom.current = map.getZoom()
+      setIconScaling(mapIconScaling(map))
+    }
+  })
 
   useMapEvent('zoomend', () => {
     setIconScaling(mapIconScaling(map))
