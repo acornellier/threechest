c677bf8645af032e253c299f054be46fe8376105
Alex Cornellier <alex@jintronix.com>
2024-03-11 19:50:51 -0400

    exclusive context menu

--- src/components/Common/useContextMenu.ts
+++ src/components/Common/useContextMenu.ts
@@ -1,33 +1,41 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import { minContextMenuWidth } from '../Sidebar/Pulls/PullContextMenu.tsx'
 import { ContextMenuPosition } from './ContextMenu.tsx'
+import { makeUseExclusiveState } from '../../hooks/exclusiveState.ts'
+
+const useExclusiveState = makeUseExclusiveState<ContextMenuPosition>()
 
 export function useContextMenu() {
-  const [position, setPosition] = useState<ContextMenuPosition | null>(null)
+  const [position, setPosition] = useExclusiveState()
 
   const onClose = useCallback(() => {
     setPosition(null)
-  }, [])
+  }, [setPosition])
 
-  const onRightClick = useCallback((e: MouseEvent) => {
-    const windowWidth = window.innerWidth
-    const newPos = {
-      top: e.pageY,
-      left:
-        e.pageX + minContextMenuWidth < windowWidth ? e.pageX : windowWidth - minContextMenuWidth,
-    }
+  const onRightClick = useCallback(
+    (e: MouseEvent) => {
+      const windowWidth = window.innerWidth
+      const newPos = {
+        top: e.pageY,
+        left:
+          e.pageX + minContextMenuWidth < windowWidth ? e.pageX : windowWidth - minContextMenuWidth,
+      }
 
-    setPosition((pos) => {
-      if (!pos) return newPos
-      const a = newPos.top - pos.top
-      const b = newPos.left - pos.left
-      return Math.hypot(a, b) < 20 ? null : newPos
-    })
-  }, [])
+      setPosition((pos) => {
+        if (!pos) return newPos
+        const a = newPos.top - pos.top
+        const b = newPos.left - pos.left
+        return Math.hypot(a, b) < 20 ? null : newPos
+      })
+    },
+    [setPosition],
+  )
 
   useEffect(() => {
     document.addEventListener('click', onClose)
-    return () => document.removeEventListener('click', onClose)
+    return () => {
+      document.removeEventListener('click', onClose)
+    }
   }, [onClose])
 
   return { contextMenuPosition: position, onRightClick, onClose }
---
+++ src/hooks/exclusiveState.ts
@@ -0,0 +1,26 @@
+﻿import { Dispatch, SetStateAction, useLayoutEffect, useState } from 'react'
+
+const noOp = () => {}
+
+export function makeUseExclusiveState<T>(defaultValue: T | null = null) {
+  let setLastUsedState: Dispatch<SetStateAction<T | null>> = noOp
+
+  return () => {
+    const [state, setState] = useState(defaultValue)
+
+    useLayoutEffect(() => {
+      if (state && setLastUsedState !== setState) {
+        setLastUsedState(defaultValue)
+        setLastUsedState = setState
+      }
+
+      return () => {
+        if (setLastUsedState === setState) {
+          setLastUsedState = noOp
+        }
+      }
+    }, [state, setState])
+
+    return [state, setState] as const
+  }
+}
