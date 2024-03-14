2b90b44f85701cdaaf18932d9309f6f904bf4b50
Alex Cornellier <alex@jintronix.com>
2024-03-14 12:05:05 -0400

    fix spells

--- src/data/spells.ts
+++ src/data/spells.ts
@@ -4,21 +4,21 @@ import { ebSpells } from './spells/ebSpells.ts'
 import { brhSpells } from './spells/brhSpells.ts'
 import { dhtSpells } from './spells/dhtSpells.ts'
 import { fallSpells } from './spells/fallSpells.ts'
-import { riseSpells } from './spells/riseSpells.ts'
 import { tottSpells } from './spells/tottSpells.ts'
 import { wcmSpells } from './spells/wcmSpells.ts'
 import { nokSpells } from './spells/nok.ts'
+import { riseSpells } from './spells/riseSpells.ts'
 
 export const dungeonSpells: Record<DungeonKey, Spells> = {
   aa: {}, // TODO
   ad: adSpells,
   bh: {}, // TODO
-  brh: ebSpells,
-  dht: brhSpells,
-  fall: dhtSpells,
-  rise: fallSpells,
-  eb: riseSpells,
+  brh: brhSpells,
+  dht: dhtSpells,
+  fall: fallSpells,
+  eb: ebSpells,
   nok: nokSpells,
+  rise: riseSpells,
   tott: tottSpells,
   wcm: wcmSpells,
 }
