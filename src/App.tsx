0e1de03d42ad7a49df382a59b4acc3fee81ca1bc
Alex <alex@jintronix.com>
2024-03-15 00:20:35 -0400

    disable gridlines fix on mac

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import { persistor, store } from './store/store.ts'
 import '@fontsource-variable/roboto-slab'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ErrorBoundary } from './components/Common/ErrorBoundary.tsx'
+import { useEffect } from 'react'
+import { isMac } from './util/dev.ts'
 
 export default function App() {
+  useEffect(() => {
+    // need this style only on windows to fix grid lines
+    if (isMac) return
+    const style = document.createElement('style')
+    style.textContent = 'img.leaflet-tile { mix-blend-mode: normal !important; }'
+    document.head.appendChild(style)
+  }, [])
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
