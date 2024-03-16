60eee8d5d98420e828893106e90e1c0c39c29d0d
Alex <alex@jintronix.com>
2024-03-16 13:07:46 -0400

    title and temp logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,6 +8,7 @@ export function Header() {
       <div className="pointer-events-none flex flex-col items-start gap-4 lg:grid lg:items-center lg:grid-cols-[1fr_auto_1fr]">
         <div className="w-fit flex gap-4 flex-col items-start sm:flex-row sm:items-stretch pointer-events-auto">
           <Button twoDimensional innerClass="text-2xl">
+            <img src="/favicon.png" alt="logo" width={40} />
             Threechest
           </Button>
           <DungeonDropdown />
