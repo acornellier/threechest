928edd49344ca794e48268069bef0864650f7d11
Alex <alex@jintronix.com>
2024-03-17 18:59:05 -0400

    disable google analytics in dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles.scss'
 import 'leaflet/dist/leaflet.css'
+import TagManager from 'react-gtm-module'
 // import { StrictMode } from 'react'
+
+if (process.env.NODE_ENV === 'production') TagManager.initialize({ gtmId: 'G-VZFWC5SP1E' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <StrictMode>
