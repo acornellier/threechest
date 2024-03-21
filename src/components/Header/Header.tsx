9864613a1646d3e2de20834f3e77b72c0ce130b3
Alex Cornellier <alex@jintronix.com>
2024-03-21 13:51:23 -0400

    new logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-﻿import { DungeonDropdown } from './DungeonDropdown.tsx'
+import { DungeonDropdown } from './DungeonDropdown.tsx'
 import { Button } from '../Common/Button.tsx'
 import { UndoRedo } from './UndoRedo.tsx'
 
@@ -12,7 +12,7 @@ export function Header() {
       <div className="pointer-events-none flex flex-col items-start gap-4 lg:grid lg:items-center lg:grid-cols-[minmax(470px,1fr)_auto_1fr]">
         <div className="w-fit flex gap-4 flex-col items-start sm:flex-row sm:items-stretch pointer-events-auto">
           <Button twoDimensional className="min-w-fit" innerClass="text-2xl">
-            <img src="/images/favicon.png" alt="logo" width={36} className="min-w-[36]" />
+            <img src="/images/favicon2.png" alt="logo" width={36} className="min-w-[36] mr-1" />
             <div>Threechest</div>
           </Button>
           <DungeonDropdown />
