fa1f45d5a16b82a96ed26775c5ff8c68205bbf17
Alex <alex@jintronix.com>
2024-03-24 22:17:14 -0400

    styling

--- src/components/Collab/CollabRoomDetails.tsx
+++ src/components/Collab/CollabRoomDetails.tsx
@@ -15,51 +15,49 @@ export function CollabRoomDetails() {
   const room = useCollabSelector((state) => state.room)
   const awarenessStates = useAwarenessStates()
   const [showClients, setShowClients] = useState(false)
+
+  const awarenessLoaded = awarenessStates.length > 0
   const local = awarenessStates.find(({ isCurrentClient }) => isCurrentClient)
   const isHost = local?.clientType === 'host'
 
   return (
     <div className="flex flex-col gap-1">
       <div className="text-xs -mt-1">Room: {room}</div>
-      <div className="flex flex-wrap justify-between gap-2 text-xs">
-        {local && (
-          <div
-            className="flex items-center rounded-sm px-1"
-            data-tooltip-id="collab-self-name-tooltip"
-            style={{
-              backgroundColor: local.color ?? 'none',
-              color: local.color ? getTextColor(local.color) : 'inherit',
-            }}
-          >
-            You: {local.name ?? 'No name'} <b className="ml-1">{local.clientType}</b>
-          </div>
-        )}
-        {awarenessStates.length > 0 && (
-          <div className="flex items-center gap-1">
-            <Button
-              tiny
-              twoDimensional
-              outline
-              Icon={ChevronDownIcon}
-              iconSize={14}
-              className="cursor-default whitespace-nowrap"
-              data-tooltip-id="collab-guests-tooltip"
-              onClick={() => setShowClients((v) => !v)}
+      {awarenessLoaded && (
+        <div className="flex flex-wrap justify-between gap-2 text-xs">
+          {local && (
+            <div
+              className="flex items-center rounded-sm px-1"
+              data-tooltip-id="collab-self-name-tooltip"
+              style={{
+                backgroundColor: local.color ?? 'none',
+                color: local.color ? getTextColor(local.color) : 'inherit',
+              }}
             >
-              Clients: {awarenessStates.length}
-            </Button>
-          </div>
-        )}
-      </div>
+              You: {local.name ?? 'No name'} <b className="ml-1">{local.clientType}</b>
+            </div>
+          )}
+          <Button
+            tiny
+            twoDimensional
+            outline
+            Icon={ChevronDownIcon}
+            iconSize={14}
+            className="cursor-default whitespace-nowrap"
+            data-tooltip-id="collab-guests-tooltip"
+            onClick={() => setShowClients((v) => !v)}
+          >
+            Clients: {awarenessStates.length}
+          </Button>
+        </div>
+      )}
       <TooltipStyled id="collab-self-name-tooltip" place="bottom">
         {isHost ? (
           <>
             <p>You are the host of this collab room.</p>
             <p>Only you can change the dungeon or route.</p>
-            <p>
-              If you are unhappy with the changes your guests have made, you can restore your route
-              from a backup using the Restore button.
-            </p>
+            <p>You can promote another client to be host using the clients list on the right.</p>
+            <p>You can restore your route from a backup using the Restore button.</p>
             <p>If you leave, the person who joined the earliest will be promoted to host.</p>
           </>
         ) : (
