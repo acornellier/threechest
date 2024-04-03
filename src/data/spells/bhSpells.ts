2a7a9ef80abda6f6f9d3bcaefaf9a592f5d89cec
Alex Cornellier <alex@jintronix.com>
2024-04-02 23:01:18 -0400

    bh

--- src/data/spells/bhSpells.ts
+++ src/data/spells/bhSpells.ts
@@ -1,1 +1,22 @@
-export default {}
+import spells from './bh_spells.json'
+import { Spells } from '../types.ts'
+import { mergeSpells } from './mergeSpells.ts'
+
+const extraSpells: Spells = {
+  186121: [
+    {
+      id: 373939,
+      name: 'Rotting Burst',
+      icon: 'sha_spell_shaman_lavaburst_nightmare',
+    },
+    {
+      id: 387264,
+      name: 'Withered Eruption',
+      icon: 'warlock_curse_weakness_aura',
+    },
+  ],
+}
+
+const removedSpells = [373915, 373917, 385361]
+
+export default mergeSpells(spells, extraSpells, removedSpells)
