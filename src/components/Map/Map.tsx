5fa099a832f2d8ffe728bf601d2a87120004e4cf
Alex <alex@jintronix.com>
2024-04-10 12:41:18 -0400

    compress map images

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -55,7 +55,7 @@ export function Map() {
           noWrap
           minNativeZoom={2}
           maxNativeZoom={2}
-          url={`/maps/${dungeon.key}/{z}/{x}_{y}.png`}
+          url={`/maps/${dungeon.key}/{z}/{x}_{y}.jpg`}
         />
         <Mobs />
         <PullOutlines />
