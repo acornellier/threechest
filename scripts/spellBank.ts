294041261595294fe40e6e899fc5df01b3c93a7d
Alex <alex@jintronix.com>
2024-04-14 23:37:47 -0400

    update spell bank

---
+++ scripts/spellBank.ts
@@ -0,0 +1,13 @@
+import { getDirname } from '../server/files.ts'
+import fs from 'fs/promises'
+
+const dirname = getDirname(import.meta.url)
+
+const data = await fetch('https://keyandheal.com/page-data/dungeons/SpellBank_S4/page-data.json')
+const spellBank = await data.json()
+
+await fs.writeFile(
+  `${dirname}/../src/data/spells/spellBank.json`,
+  JSON.stringify(spellBank, null, 2),
+  'utf-8',
+)
