fdce730b5d77864b643ee76e56ec858596bafaaf
Alex <alex@jintronix.com>
2024-04-16 09:36:33 -0400

    reduce nelth boss spells

--- src/data/spells/nelth/nelthSpells.ts
+++ src/data/spells/nelth/nelthSpells.ts
@@ -3,13 +3,32 @@ import type { SpellIdMap } from '../../types.ts'
 import { mergeSpells } from '../grimoire.ts'
 
 const extraSpells: SpellIdMap = {
-  181861: [375890], // Elephant boss - Magma Eruption
-  189340: [373756, 373767], // Forgemaster
-  189478: [], // Forgemaster
-  189901: [376783, 391762], // Final boss intermission damage, curse
+  181861: [375449], // Elephant boss - Charge
+  189340: [374482, 414585], // Chargath - Grounding Chain, Fiery Demise
+  189901: [391762], // Final boss intermission damage, curse
 }
 
-const removedSpells: number[] = []
+const removedSpells: number[] = [
+  375436, // Elephant Charge
+  373762, // Chargath Magma Wave
+  373767, // Chargath Magma Wave
+  373424, // Chargath Grounding Spear
+  375057, // Chargath Grounding Spear
+  396424, // Chargath Grounding Spear
+  375055, // Chargath Fiery Focus
+  384019, // Chargath Fiery Focus
+  396332, // Chargath Fiery Focus
+  437458, // Chargath Fiery Focus
+  374812, // Forgemaster Blazing Aegis
+  374812, // Forgemaster Blazing Aegis
+  374842, // Forgemaster Blazing Aegis
+  392666, // Forgemaster Blazing Aegis
+  377995, // Forgemaster Forgestorm
+  374534, // Forgemaster Heated Swings
+  374535, // Forgemaster Heated Swings
+  375306, // Forgemaster Heated Swings
+  374635, // Forgemaster Might of the Forge
+]
 
 export default async () => ({
   data: mergeSpells(spells as SpellIdMap, extraSpells, removedSpells),
