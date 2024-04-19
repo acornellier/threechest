b65032b29fd88ce90bf73107599a9cf0e1954691
Alex Cornellier <alex@jintronix.com>
2024-04-19 17:56:13 -0400

    convert wclRoute api to ts

--- api/wclRoute.ts
+++ api/wclRoute.ts
@@ -1,6 +1,6 @@
 import { getWclRoute } from '../server/wclRoute.ts'
 
-export default async function wclRouteApi(request, response) {
+export default async function wclRouteApi(request: any, response: any) {
   const route = await getWclRoute(request.body.code, request.body.fightId)
   response.status(200).json(route)
 }
