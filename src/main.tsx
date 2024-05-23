b5b54664b11544bc73405f225e2e81c0dde1674e
Alex <alex@jintronix.com>
2024-05-23 09:32:50 -0400

    change gtag

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@ import './styles/roboto-slab.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/styles.scss'
-import TagManager from 'react-gtm-module'
 // import { StrictMode } from 'react'
-
-if (process.env.NODE_ENV === 'production') TagManager.initialize({ gtmId: 'G-VZFWC5SP1E' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <StrictMode>
