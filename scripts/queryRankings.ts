898cea11f8b4d99d81cf45416fab80f2cded55f8
Alex <alex@jintronix.com>
2024-09-18 10:23:17 -0400

    refactor rankings

--- scripts/queryRankings.ts
+++ scripts/queryRankings.ts
@@ -7,20 +7,39 @@ import { fetchTopRankings } from '../server/wclRankingsFetcher.ts'
 import type { SampleRoute } from '../src/util/types.ts'
 import * as path from 'path'
 import { shuffle } from '../src/util/nodash.ts'
+import type { WclRanking } from '../src/util/wclRankings.ts'
+import type { DungeonKey } from '../src/data/dungeonKeys.ts'
 
 const dirname = getDirname(import.meta.url)
 
 const toFileName = (report: { code: string; fightID: number }) =>
   `${report.code}-${report.fightID}.json`
 
+interface DungeonRankings {
+  dungeonKey: DungeonKey
+  dungeonFolder: string
+  rankings: WclRanking[]
+}
+
+const dungeonRankings: DungeonRankings[] = []
 for (const dungeon of shuffle(dungeons)) {
   if (!dungeon.wclEncounterId) continue
 
-  console.log(`Querying dungeon ${dungeon.key}`)
   const dungeonFolder = `${dirname}/../src/data/sampleRoutes/${dungeon.key}`
+  if (!fs.existsSync(dungeonFolder)) fs.mkdirSync(dungeonFolder)
+
+  console.log(`Querying dungeon ${dungeon.key}`)
   const rankings = await fetchTopRankings(dungeon.wclEncounterId)
 
-  if (!fs.existsSync(dungeonFolder)) fs.mkdirSync(dungeonFolder)
+  dungeonRankings.push({
+    dungeonKey: dungeon.key,
+    dungeonFolder,
+    rankings,
+  })
+}
+
+for (const { dungeonFolder, rankings, dungeonKey } of dungeonRankings) {
+  console.log(`Updating and adding rankings for ${dungeonKey}`)
 
   // Remove old rankings
   for (const file of fs.readdirSync(dungeonFolder)) {
@@ -39,8 +58,13 @@ for (const dungeon of shuffle(dungeons)) {
     const oldRank = sampleRoute.wclRanking?.rank
     sampleRoute.wclRanking = ranking
     fs.writeFileSync(file, JSON.stringify(sampleRoute))
-    console.log(`Updated ${code}-${fightID} from rank ${oldRank} to ${ranking.rank}`)
+    if (oldRank !== ranking.rank)
+      console.log(`Updated ${code}-${fightID} from rank ${oldRank} to ${ranking.rank}`)
   }
+}
+
+for (const { dungeonFolder, rankings, dungeonKey } of dungeonRankings) {
+  console.log(`Adding new rankings for ${dungeonKey}`)
 
   // Add new rankings
   for (const ranking of rankings) {
