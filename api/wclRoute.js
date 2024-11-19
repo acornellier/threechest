7047e35c8f80fab40c2579861af50374fe2a789e
Alex <alex@jintronix.com>
2024-11-19 08:55:56 -0500

    vercel error message on wclRoute failed

--- api/wclRoute.js
+++ api/wclRoute.js
@@ -1,6 +1,10 @@
 import { getWclRoute } from '../dist/vercelServer.js'
 
 export default async function wclRouteApi(request, response) {
-  const route = await getWclRoute(request.body.code, request.body.fightId)
-  response.status(200).json(route)
+  try {
+    const route = await getWclRoute(request.body.code, request.body.fightId)
+    response.status(200).json(route)
+  } catch (err) {
+    response.status(422).send(err.message)
+  }
 }
