b0b17afdfa3086f2a5b96434555cc14e3fb9fd1d
Alex <alex@cornellier.com>
2026-03-28 15:28:09 -0400

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { persistor, store } from './store/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ErrorBoundary } from './components/Common/ErrorBoundary.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Page />
         </ErrorBoundary>
       </PersistGate>
+      <Analytics />
     </Provider>
   )
 }
